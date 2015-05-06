4e658b78dadad1aa2b614f165e45be4d2f893819
rankun203 <rankun203@gmail.com>
2015-05-07 00:40:39 +0800

    express 测试4 good old form

--- expressworks/index.js
+++ expressworks/index.js
@@ -4,12 +4,9 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-app.set('views', path.join('./', 'templates'));
-app.set('view engine', 'jade');
-
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/home', function (req, res) {
-    res.render('index', {date: new Date().toDateString()});
+app.post('/form', function (req, res) {
+    res.end(req.body.str.split('').reverse().join(''));
 });
 app.listen(process.argv[2]);
