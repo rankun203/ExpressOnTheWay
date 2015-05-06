ee630c9f52fd196faa25d0458d8c921b9ca90a25
rankun203 <rankun203@gmail.com>
2015-05-07 01:27:34 +0800

    express 测试7 获取参数

--- expressworks/index.js
+++ expressworks/index.js
@@ -1,18 +1,17 @@
 var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var crypto = require('crypto');
+var fs = require('fs');
 
 var app = express();
 
-
-app.use(bodyParser.urlencoded({extended: false}));
-
 app.use('/', express.static('public'));
 
-// http://localhost:3000/message?id=45678765456789
-// test: /search/?results=recent&amp;type=quote&amp;page=6
-app.get('/search', function (req, res) {
-    res.send(req.query);
+app.get('/books', function (req, res) {
+    var file = /*process.argv[3]*/ 'xx';
+
+    fs.readFile(file, function(err, data) {
+        if (err) return res.sendStatus(500);
+
+        res.json(JSON.parse(data));
+    });
 });
 app.listen(process.argv[2]);
