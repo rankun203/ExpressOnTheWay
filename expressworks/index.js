0a84383a5ba4a8ce10fdb7ea349946bd656ad72d
rankun203 <rankun203@gmail.com>
2015-05-07 01:09:53 +0800

    express 测试7 获取参数

--- expressworks/index.js
+++ expressworks/index.js
@@ -13,10 +13,6 @@ app.use('/', express.static('public'));
 // http://localhost:3000/message?id=45678765456789
 // test: /search/?results=recent&amp;type=quote&amp;page=6
 app.get('/search', function (req, res) {
-    var rtn = crypto.createHash('sha1')
-        .update(new Date().toDateString() + req.query.id)
-        .digest('hex');
-
     res.send({
         results: req.query.results,
         type: req.query.type,
