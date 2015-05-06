0e4b3bc7e219597ff6ac757e3841d95a7de4dad8
rankun203 <rankun203@gmail.com>
2015-05-07 00:59:34 +0800

    express 测试6 参数传递

--- expressworks/index.js
+++ expressworks/index.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var crypto = require('crypto');
 
 var app = express();
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(require('stylus').middleware('public'));
 
 app.use('/', express.static('public'));
 
-app.post('/form', function (req, res) {
-    res.end(req.body.str.split('').reverse().join(''));
+app.put('/message/:id', function (req, res) {
+    var rtn = crypto.createHash('sha1')
+        .update(new Date().toDateString() + req.params.id)
+        .digest('hex');
+
+    res.end(rtn);
 });
 app.listen(process.argv[2]);
