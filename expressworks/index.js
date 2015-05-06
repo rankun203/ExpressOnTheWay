7b030a4e5ce719a98206c636dcf11d2f7950a544
rankun203 <rankun203@gmail.com>
2015-05-06 23:58:50 +0800

    express 测试2 static

--- expressworks/index.js
+++ expressworks/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(process.argv[3] || path('index.html')));
+
 app.get('/home', function(req, res) {
     res.end('Hello World!');
 });
